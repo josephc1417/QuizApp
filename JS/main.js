37fe82a3a0059d560573c39c97235fc3e885fcef
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-06-29 13:18:43 -0400

    Fist commit for QuizApp

---
+++ JS/main.js
@@ -0,0 +1,43 @@
+
+// each question is going to be an obj literal within the array 
+
+const quizData = [
+    {
+        question: 'How old is Linda',
+        a_1:'10',
+        a_2:'17',
+        a_3:'26',
+        a_4:'110',
+        correct:'c'
+    }, {
+        question: 'What is the most used programming language in 2023?',
+        a:'Java',
+        b:'C',
+        c:'Python',
+        d:'JavaScript',
+        correct:'d'
+    },{
+        question: "Who is the president of the US?",
+        a:'Trump',
+        b:'Sleepy Joe',
+        c:'Obama',
+        d:'Jason Kid',
+        correct:'a'
+    },{
+        question: "What does HTML stand for?",
+        a:'Hypertext Markup Language',
+        b:'Happytimes making Love',
+        c:'Hypertext moveable Language',
+        d:'Hexadecimal-text Multi-variable Language',
+        correct:'a'
+    },{
+        question: "What year was JavaScript Launched?",
+        a:'1995',
+        b:'1996',
+        c:'1994',
+        d:'none of the above',
+        correct:'a'
+    }
+
+
+]
